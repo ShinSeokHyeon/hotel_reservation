965b0ede82e7d735cb51a381cb487b39431e7938
ShinSeokHyeon <88864523+ShinSeokHyeon@users.noreply.github.com>
2021-09-20 03:47:18 +0900

    Delete hotel directory

--- hotel/src/main/java/hotelreservation/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package hotelreservation;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel="hotels", path="hotels")
-public interface HotelRepository extends PagingAndSortingRepository<Hotel, Long>{
-
-}
