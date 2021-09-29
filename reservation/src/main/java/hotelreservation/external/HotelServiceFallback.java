6968fa66c10c4b2c1e10a6cf4ff6967291d3db6a
ShinSeokHyeon <88864523+ShinSeokHyeon@users.noreply.github.com>
2021-09-30 00:53:40 +0900

    Update HotelServiceFallback.java

--- reservation/src/main/java/hotelreservation/external/HotelServiceFallback.java
+++ reservation/src/main/java/hotelreservation/external/HotelServiceFallback.java
@@ -9,8 +9,12 @@ public class HotelServiceFallback implements HotelService {
     // circuit breaker 발동 시 Hotel을 null값으로 리턴하며 "Circuit breaker has been opened. Fallback returned instead." 문구 출력
     @Override
     public Hotel getHotelStatus(Long id) {
-        System.out.println("Circuit breaker has been opened. Fallback returned instead.");
-        return false;
+        System.out.println("########### Circuit breaker has been opened. Fallback returned instead. ###########");
+        System.out.println("########### Circuit breaker has been opened. Fallback returned instead. ###########");
+        System.out.println("########### Circuit breaker has been opened. Fallback returned instead. ###########");
+        System.out.println("########### Circuit breaker has been opened. Fallback returned instead. ###########");
+        System.out.println("########### Circuit breaker has been opened. Fallback returned instead. ###########");
+        return null;
     }
 
 }
