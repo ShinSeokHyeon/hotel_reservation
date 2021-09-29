5517eaed21491cd7dadfe0860dfd2904d7a294f9
ShinSeokHyeon <88864523+ShinSeokHyeon@users.noreply.github.com>
2021-09-29 23:26:30 +0900

    Update HotelServiceFallback.java

--- reservation/src/main/java/hotelreservation/external/HotelServiceFallback.java
+++ reservation/src/main/java/hotelreservation/external/HotelServiceFallback.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class HotelServiceFallback implements HotelService {
 
+    // circuit breaker 발동 시 Hotel을 null값으로 리턴하며 "Circuit breaker has been opened. Fallback returned instead." 문구 출력
     @Override
     public Hotel getHotelStatus(Long id) {
         System.out.println("Circuit breaker has been opened. Fallback returned instead.");
