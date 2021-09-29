768fc7e572d30d002015f8811780ea5f59ea4e1a
ShinSeokHyeon <88864523+ShinSeokHyeon@users.noreply.github.com>
2021-09-29 23:24:19 +0900

    Update HotelController.java

--- hotel/src/main/java/hotelreservation/HotelController.java
+++ hotel/src/main/java/hotelreservation/HotelController.java
@@ -1,17 +1,30 @@
 package hotelreservation;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
  @RestController
  public class HotelController {
 
+	 	private HotelRepository repository;
+
+	    public HotelController(HotelRepository repository){
+	        this.repository = repository;
+	    }
+	    
+	 // getResortStatus get 호출 시 400밀리초 ~ 620밀리초의 지연시간 발생시킴
+	    @RequestMapping(method= RequestMethod.GET, value="/resorts/{id}")
+	    public Hotel getHotelStatus(@PathVariable("id") Long id){
+
+	        //hystix test code
+	        try {
+	              Thread.currentThread().sleep((long) (400 + Math.random() * 220)); // (+ 0~1*220)
+	         } catch (InterruptedException e) { }
+
+	        return repository.findById(id).get();
+	    }
 
 
  }
