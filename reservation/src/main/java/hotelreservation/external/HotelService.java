7b20b3f46dfa22c4c6413f5ed2c0948989c09d44
ShinSeokHyeon <88864523+ShinSeokHyeon@users.noreply.github.com>
2021-09-30 15:04:49 +0900

    Update HotelService.java

--- reservation/src/main/java/hotelreservation/external/HotelService.java
+++ reservation/src/main/java/hotelreservation/external/HotelService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Date;
 
-@FeignClient(name="hotel", url="${feign.hotel.url}")
+@FeignClient(name="hotel", url="${feign.hotel.url}", fallback = HotelServiceFallback.class)
 public interface HotelService {
 
     @RequestMapping(method= RequestMethod.GET, value="/hotels/{id}", consumes = "application/json")
